513753ff1c2e0c02f4a64197304329bde20cbe4c
Brandon "Okidokie98" de Baat <36400524+Okidokie98@users.noreply.github.com>
2024-06-24 16:24:05 +0200

    Update team.js

--- public/team.js
+++ public/team.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (selectedTeam) {
         // Use the proxy URL
-        fetch(`http://localhost:8080/https://api-web.nhle.com/v1/club-stats/${selectedTeamAbbreviation}/20232024/2`)
+        fetch(`https://api-web.nhle.com/v1/club-stats/${selectedTeamAbbreviation}/20232024/2`)
           .then(response => response.json())
           .then(data => {
             console.log('Data fetched:', data);
